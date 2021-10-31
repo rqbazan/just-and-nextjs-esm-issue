0102a095123c37cf0419c47f3c7ae21efd47deb2
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-10-31 11:50:11 -0500

    Add just-map-values import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import mapValues from 'just-map-values'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
